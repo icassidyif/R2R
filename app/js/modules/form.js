b88cc8c5b3db51d02f22c6157bc6043163191a21
Ihor <icassidyif@gmail.com>
2020-11-24 01:46:30 +0200

    complexes page done dev

--- app/js/modules/form.js
+++ app/js/modules/form.js
@@ -1,22 +1,22 @@
 window.addEventListener('DOMContentLoaded', () => {
 
 //  select all reset all
-  const selectsAll = document.querySelectorAll('.block-search__control .select-all');
+  const selectsAll = document.querySelectorAll('.select-all');
   selectsAll.forEach(selectAll => {
     selectAll.addEventListener('click', e => {
       e.preventDefault();
-      const parentBlock = selectAll.closest('.block-search');
+      const parentBlock = selectAll.closest('.filter__block');
       const checkboxes = parentBlock.querySelectorAll('.input-checkbox input');
       checkboxes.forEach(checkbox => {
         checkbox.checked = true;
       })
     })
   })
-  const resetsAll = document.querySelectorAll('.block-search__control .reset-all');
+  const resetsAll = document.querySelectorAll('.reset-all');
   resetsAll.forEach(resetAll => {
     resetAll.addEventListener('click', e => {
       e.preventDefault();
-      const parentBlock = resetAll.closest('.block-search');
+      const parentBlock = resetAll.closest('.filter__block');
       const checkboxes = parentBlock.querySelectorAll('.input-checkbox input');
       checkboxes.forEach(checkbox => {
         checkbox.checked = false;
